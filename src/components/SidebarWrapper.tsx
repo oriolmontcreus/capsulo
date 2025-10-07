abefed932a7f2eefd4a943d778335d83ebaf6c57
oriolmontcreus <oriolmontcreus@gmail.com>
2025-10-07 21:53:30 +0200

    feat: implement sidebarwrapper using an Astro Island

---
+++ src/components/SidebarWrapper.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+export default function SidebarWrapper({ children }: { children: React.ReactNode }) {
+    return (
+        <SidebarProvider
+            style={
+                {
+                    "--sidebar-width": "350px",
+                } as React.CSSProperties
+            }
+        >
+            <AppSidebar />
+            <SidebarInset>
+                <header className="bg-background sticky top-0 flex shrink-0 items-center gap-2 border-b p-4">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator
+                        orientation="vertical"
+                        className="mr-2 data-[orientation=vertical]:h-4"
+                    />
+                    <Breadcrumb>
+                        <BreadcrumbList>
+                            <BreadcrumbItem className="hidden md:block">
+                                <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator className="hidden md:block" />
+                            <BreadcrumbItem>
+                                <BreadcrumbPage>Home</BreadcrumbPage>
+                            </BreadcrumbItem>
+                        </BreadcrumbList>
+                    </Breadcrumb>
+                </header>
+                <div className="flex flex-1 flex-col gap-4 p-4">
+                    {children}
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
+    );
+}
